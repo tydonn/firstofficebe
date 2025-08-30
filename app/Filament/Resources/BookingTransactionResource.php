0cd9a735550cbc3463c18b6e4d416f1ff44d5fdd
tydonn <ariknurwantoh@gmail.com>
2025-08-30 14:50:42 +0700

    Menambahkan fitur Notifikasi transaksi pembayaran diproses dan Approve pembayaran melalui Whatsapp dan SMS kepada Customer

--- app/Filament/Resources/BookingTransactionResource.php
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -7,11 +7,13 @@ use App\Filament\Resources\BookingTransactionResource\RelationManagers;
 use App\Models\BookingTransaction;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Twilio\Rest\Client;
 
 class BookingTransactionResource extends Resource
 {
@@ -25,46 +27,46 @@ class BookingTransactionResource extends Resource
             ->schema([
                 //
                 Forms\Components\TextInput::make('name')
-                ->required()
-                ->maxLength(255),
+                    ->required()
+                    ->maxLength(255),
 
                 Forms\Components\TextInput::make('booking_trx_id')
-                ->required()
-                ->maxLength(255),
+                    ->required()
+                    ->maxLength(255),
 
                 Forms\Components\TextInput::make('phone_number')
-                ->required()
-                ->maxLength(255),
+                    ->required()
+                    ->maxLength(255),
 
                 Forms\Components\TextInput::make('total_amount')
-                ->required()
-                ->numeric()
-                ->prefix('IDR'),
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR'),
 
                 Forms\Components\TextInput::make('duration')
-                ->required()
-                ->numeric()
-                ->prefix('Days'),
+                    ->required()
+                    ->numeric()
+                    ->prefix('Days'),
 
                 Forms\Components\DatePicker::make('started_at')
-                ->required(),
+                    ->required(),
 
                 Forms\Components\DatePicker::make('ended_at')
-                ->required(),
+                    ->required(),
 
                 Forms\Components\Select::make('is_paid')
-                ->options([
-                    true => 'Paid',
-                    false => 'Not Paid',
-                ])
-                ->required(),
+                    ->options([
+                        true => 'Paid',
+                        false => 'Not Paid',
+                    ])
+                    ->required(),
 
                 Forms\Components\Select::make('office_space_id')
-                ->relationship('officeSpace', 'name')
-                ->searchable()
-                ->preload()
-                ->required(),
-                
+                    ->relationship('officeSpace', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
             ]);
     }
 
@@ -74,23 +76,23 @@ class BookingTransactionResource extends Resource
             ->columns([
                 //
                 Tables\Columns\TextColumn::make('booking_trx_id')
-                ->searchable(),
+                    ->searchable(),
 
                 Tables\Columns\TextColumn::make('name')
-                ->searchable(),
+                    ->searchable(),
 
                 Tables\Columns\TextColumn::make('officeSpace.name'),
 
                 Tables\Columns\TextColumn::make('started_at')
-                ->date(),
+                    ->date(),
 
                 Tables\Columns\IconColumn::make('is_paid')
-                ->boolean()
-                ->trueColor('succes')
-                ->falseColor('danger')
-                ->trueIcon('heroicon-o-check-circle')
-                ->falseIcon('heroicon-o-x-circle')
-                ->label('Sudah Bayar?'),
+                    ->boolean()
+                    ->trueColor('succes')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Sudah Bayar?'),
 
             ])
             ->filters([
@@ -98,6 +100,54 @@ class BookingTransactionResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+
+                Tables\Actions\Action::make('approve')
+                    ->label('Approve')
+                    ->action(function (BookingTransaction $record) {
+                        $record->is_paid = true;
+                        $record->save();
+
+                        //trigger the custom notiffication
+                        Notification::make()
+                            ->title('Booking Transaction Approved.')
+                            ->success()
+                            ->body('The booking transaction has been approved.')
+                            ->send();
+
+                        //mengirim notif melalui whatsapp dan sms dengan twilio
+                        // Find your Account SID and Auth Token at twilio.com/console
+                        // and set the environment variables. See http://twil.io/secure
+                        /* $sid = getenv("TWILIO_ACCOUNT_SID");
+                        $token = getenv("TWILIO_AUTH_TOKEN");
+                        $twilio = new Client($sid, $token);
+
+                        $messageBody = "Hi {$record->name}, Pemesanan Anda dengan kode {$record->booking_trx_id} sudah terbayar penuh.\n\n";
+                        $messageBody .= "Silahkan datang pada lokasi kantor {$record->office->name} untuk mulai menggunakan ruangan kerja tersebut.\n\n";
+                        $messageBody .= "Jika anda memiliki masalah silahkan hubungi cs kami"; */
+
+                        //kirim fitur sms
+                        /* $message = $twilio->messages->create(
+                            //"+689675594343"
+                            "+{$record->phone_number}",
+                            [
+                                "body" => $messageBody,
+                                "from" => getenv("TWILIO_PHONE_NUMBER")
+                            ]
+                        ); */
+
+                        //kirim fitur whatsapp
+                        /* $message = $twilio->messages
+                            ->create(
+                                "+{$record->phone_number}",
+                                array(
+                                    "from" => "whatsapp:+14155238886",
+                                    "body" => $messageBody,
+                                )
+                            ); */
+                    })
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->visible(fn(BookingTransaction $record) => !$record->is_paid)
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -10,21 +10,23 @@ use App\Models\BookingTransaction;
 use App\Models\OfficeSpace;
 use Doctrine\DBAL\Schema\View;
 use Illuminate\Http\Request;
+use Twilio\Rest\Client;
 
 class BookingTransactionController extends Controller
 {
     //Method untuk menampilkan detail transaksi booking
-    public function booking_details(Request $request) {
-        
+    public function booking_details(Request $request)
+    {
+
         $request->validate([
             'phone_number' => 'required|string',
             'booking_trx_id' => 'required|string',
         ]);
 
         $booking = BookingTransaction::where('phone_number', $request->phone_number)
-        ->where('booking_trx_id', $request->booking_trx_id)
-        ->with(['officeSpace', 'officeSpace.city'])
-        ->first();
+            ->where('booking_trx_id', $request->booking_trx_id)
+            ->with(['officeSpace', 'officeSpace.city'])
+            ->first();
 
         if (!$booking) {
             return response()->json(['message' => 'Booking not found'], 404);
@@ -57,9 +59,37 @@ class BookingTransactionController extends Controller
         $bookingTransaction = BookingTransaction::create($validatedData);
 
         //mengirim notif melalui whatsapp dan sms dengan twilio
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        /* $sid = getenv("TWILIO_ACCOUNT_SID");
+        $token = getenv("TWILIO_AUTH_TOKEN");
+        $twilio = new Client($sid, $token);
+
+        $messageBody = "Hi {$bookingTransaction->name}, Terimakasih telah booking kantor di FirstOffice.\n\n";
+        $messageBody = "Pesanan kantor {$bookingTransaction->office->name}, Anda sedang kami proses dengan Booking TRX ID {$bookingTransaction->booking_trx_id}.\n\n";
+        $messageBody = "Kami akan menginformasikan status pesanan Anda secepat mungkin.";
+
+        //kirim fitur sms
+        $twilio->messages->create(
+            //"+689675594343"
+            "+{$bookingTransaction->phone_number}",
+            [
+                "body" => $messageBody,
+                "from" => getenv("TWILIO_PHONE_NUMBER")
+            ]
+            ); */
+        
+        //kirim fitur whatsapp
+        /* $message = $twilio->messages
+        ->create(
+            "+{$bookingTransaction->phone_number}",
+            array(
+                "from" => "whatsapp:+14155238886",
+                "body" => $messageBody,
+            )
+            ); */
 
         //mengembalikan response hasil transaksi
-
         $bookingTransaction->load('officeSpace');
         return new BookingTransactionResource($bookingTransaction);
 
